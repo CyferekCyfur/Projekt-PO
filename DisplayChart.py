94c4e60b684c9018d60a8d87a3aefe0b9cbd3afe
CyferekCyfur <kubapro84@wp.pl>
2022-05-25 18:40:16 +0200

    Rebuilded code

--- DisplayChart.py
+++ DisplayChart.py
@@ -1,18 +1,30 @@
 from LoadFile import LoadFile
-
+from matplotlib import pyplot as plt
 
 class DisplayChart(LoadFile):
     def __init__(self, width, height, dpi, position_x, position_y, color, label, filename, field_width, field_height):
-        super().__init__(width, height, position_x, position_y, color, label, filename, field_width, field_height)
+        super().__init__(filename)
         self.width = width
         self.height = height
         self.dpi = dpi
 
     def add_new_plot(self):
-        pass
+        file = LoadFile("Electricity prices for household consumers - bi-annual data (from 2007 onwards) [NRG_PC_204].csv")
+        dates, countries = file.load_file()
+        for country in countries.keys():
+            list_of_country_values = countries[country]
+            plt.plot(dates, list_of_country_values)
+            plt.xticks(rotation=60)
+            plt.show()
+            return 0
+
+        
 
     def set_bottom_boundary(self):
         pass
 
     def set_top_boundary(self):
         pass
+
+dc = DisplayChart(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+dc.add_new_plot()
--- LoadFile.py
+++ LoadFile.py
@@ -1,52 +1,61 @@
-from BottomButtonsPanel import BottomButtonsPanel
-
-
-class LoadFile(BottomButtonsPanel):
-    def __init__(self, width, height, position_x, position_y, color, label, filename, field_width, field_height):
-        super().__init__(field_width, field_height)
-        self.width = width
-        self.height = height
-        self.position_x = position_x
-        self.position_y = position_y
-        self.color = color
-        self.label = label
+class LoadFile:
+    def __init__(self, filename):
         self.filename = filename
 
-    def load_file(self, filename):
-        countries = []
-        with open(filename, 'r', newline="", ) as fhand:
-            lines = fhand.readlines()
+    def getFilename(self):
+        return self.filename
+
+    def load_file(self):
+        countries = {}
+        dates = []
+
+        # wyciaganie krajow z pliku oraz ich danych kt
+        with open(self.getFilename(), 'r') as fp:
+            lines = fp.readlines()
             lines_to_be_processed = lines[4:]
             for line in lines_to_be_processed:
-                words = line.strip().split(sep=",")
-                # wyciągane zostają kraje z poszczególnych linijek i zostają ustawiane jako klucze słownika
-                countries[words[0]] = 0
-                data_in_line_processed = line.strip().split("\"")
-                data_per_country = []
-                # program przechodzi przez każdą linijkę i tworzy listę z cenami energii
-                for el in data_in_line_processed:
-                    if el == "":
-                        pass
-                    elif ":" in el:
-                        new_el = el.split(",")
-                        for blank_data in new_el:
-                            if blank_data == ":":
-                                data_per_country.append(0)
-
-                    elif el.startswith("0"):
-                        formatted_el = el.replace(",", ".")
-                        data_per_country.append(formatted_el)
-                countries[words[0]] = data_per_country
-
+                country = ''
+                comma = False
+                counter = False
+                number = ''
+                list_of_strings = []
+                for character in line:
+                    if character == '\n':
+                        break
+                    if character != ',' and not comma:
+                        country += character
+                        continue
+                    else:
+                        comma = True
+                    if character == ':':
+                        list_of_strings.append('0.0')
+                        continue
+                    if character == '"' and not counter:
+                        counter = True
+                        continue
+                    if character == '"' and counter:
+                        counter = False
+                        list_of_strings.append(number)
+                        number = ''
+                        continue
+                    if character == ',' and counter:
+                        number += '.'
+                        continue
+                    if counter:
+                        number += character
+                list_of_values = []
+                for number in list_of_strings:
+                    list_of_values.append(float(number))
+                countries[country] = list_of_values
             # wyciaganie poszczegolnych dat
             line_0 = lines[0]
             formatted_line_0 = line_0.strip().split(",")
-            for el in formatted_line_0:
-                if el == "TIME":
+            for element in formatted_line_0:
+                if element == "TIME":
                     pass
                 else:
-                    dates.append(el)
+                    dates.append(element)
+            return dates, countries
 
 
-
-
+lf = LoadFile("Electricity prices for household consumers - bi-annual data (from 2007 onwards) [NRG_PC_204].csv")
